6eb79dcef87dfb262fbbd68e3115bc17c4572d0f
FahmiSMH <156737441+FahmiSMH@users.noreply.github.com>
2024-02-03 15:38:27 +0800

    edited main

--- source/main.cpp
+++ source/main.cpp
@@ -2,14 +2,44 @@
 //
 
 #include <iostream>
+#include "../header/calculator.h"
 
 using namespace std;
 
+double Calculator::Calculate(double x, char oper, double y){
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x-y;
+    case '*':
+        return x*y;
+    case '/':
+        return x/y;
+    default:
+        return 0.0;
+    }
+}
+
 int main()
 {
+    double x=0.0;
+    double y =0.0;
+    double result =0.0;
+    char oper = '+';
+
     cout << "Calculator Console Application" << endl << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
         << endl;
+    Calculator c;
+    while (true)
+    {
+        cin>>x>>oper>>y;
+        result=c.Calculate(x,oper,y);
+        cout<<"Result "<<"of "<<x<<oper<<y<<"is: "<<result;
+    }
+    
     return 0;
 }
 
