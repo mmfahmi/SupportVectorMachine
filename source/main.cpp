f51006a100e4a4496a2be181c6dec817bb74e73e
FahmiSMH <156737441+FahmiSMH@users.noreply.github.com>
2024-02-08 12:30:31 +0800

    formatting

--- source/main.cpp
+++ source/main.cpp
@@ -6,17 +6,18 @@
 
 using namespace std;
 
-double Calculator::Calculate(double x, char oper, double y){
+double Calculator::Calculate(double x, char oper, double y)
+{
     switch (oper)
     {
     case '+':
         return x + y;
     case '-':
-        return x-y;
+        return x - y;
     case '*':
-        return x*y;
+        return x * y;
     case '/':
-        return x/y;
+        return x / y;
     default:
         return 0.0;
     }
@@ -24,22 +25,23 @@ double Calculator::Calculate(double x, char oper, double y){
 
 int main()
 {
-    double x=0.0;
-    double y =0.0;
-    double result =0.0;
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
     char oper = '+';
 
-    cout << "Calculator Console Application" << endl << endl;
+    cout << "Calculator Console Application" << endl
+         << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
-        << endl;
+         << endl;
     Calculator c;
     while (true)
     {
-        cin>>x>>oper>>y;
-        result=c.Calculate(x,oper,y);
-        cout<<"Result of "<<x<<" "<<oper<<" "<<y<<" is: "<<result;
+        cin >> x >> oper >> y;
+        result = c.Calculate(x, oper, y);
+        cout << "Result of " << x << " " << oper << " " << y << " is: " << result << endl;
     }
-    
+
     return 0;
 }
 
